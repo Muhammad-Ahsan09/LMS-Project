dfbd162a6b9496152502b1ef9f486ec421e236e6
Venom <mmahsan322@gmail.com>
2025-09-06 14:07:29 -0700

    added origin to vite

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ const app = express()
 await connectCloudinary()
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+    origin: "https://lms-project-gilt-kappa.vercel.app/"
+}))
 app.use(clerkMiddleware())
 
 // Routes
